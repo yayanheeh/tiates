cf351d2a2c14a6b6790d8839204a58157e65fc7e
Fathan <s6030213033@phuket.psu.ac.th>
2019-10-14 18:02:19 +0700

    add firebase

--- js/app.js
+++ js/app.js
@@ -1,6 +1,47 @@
+ //initial firebase
+ var firebaseConfig = {
+    apiKey: "AIzaSyCRG3ZSkBe9IHoQNLPXTnIYm7fbclf9K_A",
+    authDomain: "fastfood-4d4ff.firebaseapp.com",
+    databaseURL: "https://fastfood-4d4ff.firebaseio.com",
+    projectId: "fastfood-4d4ff",
+    storageBucket: "fastfood-d742f.appspot.com",
+    messagingSenderId: "959087323418",
+    appId: "1:959087323418:web:c926ea3dd9d34a2f64a4a3"
+   };
+   firebase.initializeApp(firebaseConfig);
+
+   var db = firebase.firestore();
+
+
+// ดูสถานะการ login
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    //var displayName = user.displayName;
+    var email = user.email;
+    console.log(email + "signed in");
+    // var emailVerified = user.emailVerified;
+    // var photoURL = user.photoURL;
+    // var isAnonymous = user.isAnonymous;
+    // var uid = user.uid;
+    // var providerData = user.providerData;
+    // ...
+  } else {
+    console.log("sign out");
+    // User is signed out.
+    // ...
+  }
+});
+
+
+
+
 document.addEventListener('init', function (event) {
     var page = event.target;
     console.log(page.id);
+
+
+
 
     if (page.id === "tabbar") {
         //Code for tabbar
@@ -201,4 +242,84 @@ document.addEventListener('init', function (event) {
     }
 
 
+});
+
+
+ 
+
+
+document.addEventListener('init', function (event) {
+  var page = event.target;
+ 
+
+
+  if (page.id === 'homePage') {
+    console.log("homePage");
+
+    $("#menubtn").click(function () {
+      $("#sidemenu")[0].open();      
+    });
+
+    $("#carousel").empty();
+    db.collection("recommended").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          
+        var item = `
+        <ons-carousel-item modifier="nodivider" id="${doc.data().id}" class="recomended_item">
+            <div class="thumbnail" style="background-image: url('${doc.data().photoUrl}')">
+            </div>
+            <div class="recomended_item_title" id="item1_name">${doc.data().name}</div>
+        </ons-carousel-item>`
+
+        $("#carousel").append(item);
+
+
+      });
+    });
+
+  }
+
+  if (page.id === 'menuPage') {
+    console.log("menuPage");
+
+    $("#login").click(function () {
+      $("#content")[0].load("login.html");  
+      $("#sidemenu")[0].close();   
+    });
+
+    $("#logout").click(function () {
+      //firebase sign out
+      firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        $("#content")[0].load("login.html");  
+        $("#sidemenu")[0].close();   
+      }).catch(function(error) {
+        // An error happened.
+        console.log(error.message);
+      });
+    });
+
+    $("#home").click(function () {
+      $("#content")[0].load("home.html");  
+      $("#sidemenu")[0].close();   
+    });
+  }
+
+  if (page.id === 'loginPage') {
+    console.log("loginPage");
+
+    $("#signinbtn").click(function(){
+      var username = $("#username").val();
+      var password = $("#password").val();
+      firebase.auth().signInWithEmailAndPassword(username, password).catch(function(error) {
+
+        console.log(error.message);
+      });
+
+    })
+
+    $("#backhomebtn").click(function () {
+      $("#content")[0].load("home.html");      
+    });
+  }
 });
